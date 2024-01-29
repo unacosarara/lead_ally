cd96df4b340954fe4f775dc13a2f58f1df195887
unacosarara <peraltajoseisrrael@gmail.com>
2024-01-29 11:18:05 -0400

    0.7
    bot

---
+++ lead/script/bot_1.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const SPEED = 70
+const speed = 20
+var direccion: bool
+@export var anim: AnimationPlayer 
+@export var cast_player_left: RayCast2D
+@export var cast_player_right: RayCast2D
+@export var cast_left: RayCast2D
+@export var cast_right: RayCast2D
+@export var sprite: Sprite2D
+
+func _ready():
+	anim.play("run")
+	velocity.x = -SPEED
+	
+func _physics_process(delta):
+	
+	if cast_player_left.is_colliding():
+		sprite.flip_h = false
+		velocity.x = -speed
+		
+	elif cast_player_right.is_colliding():
+		sprite.flip_h = true
+		velocity.x = speed
+
+	if not cast_player_left.is_colliding() and !sprite.flip_h:
+		velocity.x = -SPEED
+	elif not cast_player_right.is_colliding() and sprite.flip_h: 
+		velocity.x = SPEED
+		
+		
+	if cast_left.is_colliding():
+		sprite.flip_h = true
+	elif cast_right.is_colliding():
+		sprite.flip_h = false
+		
+		
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
+	
--- lead/script/yosef.gd
+++ lead/script/yosef.gd
@@ -42,7 +42,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 													   #<>
-func _process(_delta):
-	pass
+
 
 
